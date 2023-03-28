5b4ea117d5f9a158eb475fdc8dcad6400de0b100
“Saki <sakisandra.c@gmail.com>
2023-03-27 20:20:17 -0600

    Fix make cover and home buttons and add functionality to view covers button

--- src/main.js
+++ src/main.js
@@ -7,34 +7,44 @@ var savedCovers = [
 ];
 var currentCover;
 let form = document.querySelector('form');
-let coverButton = document.querySelector('.make-new-button');
+let makeCoverButton = document.querySelector('.make-new-button');
 let homeView = document.querySelector('.home-view');
 let randCoverButton = document.querySelector('.random-cover-button');
 let saveCoverButton = document.querySelector('.save-cover-button');
 let homeButton = document.querySelector('.home-button');
+let viewSavedButton = document.querySelector('.view-saved-button')
 
-// Add your event listeners here 👇
-coverButton.addEventListener('click', coverButtonClick);
-homeButton.addEventListener('click', homeButtonClick);
 form.classList.add('hidden');
 homeButton.classList.add('hidden');
 
+// Add your event listeners here 👇
+makeCoverButton.addEventListener('click', coverButtonClick);
+homeButton.addEventListener('click', homeButtonClick);
+viewSavedButton.addEventListener('click', viewSavedCovers);
 
 // Create your event handlers and other functions here 👇
 function coverButtonClick() {
-  form.classList.toggle('hidden');
-  homeView.classList.toggle('hidden');
-  randCoverButton.classList.toggle('hidden');
-  saveCoverButton.classList.toggle('hidden');
-  homeButton.classList.toggle('hidden');
+  form.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  randCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
 }
 
 function homeButtonClick() {
-  homeButton.classList.toggle('hidden');
-  form.classList.toggle('hidden');
-  homeView.classList.toggle('hidden');
-  randCoverButton.classList.toggle('hidden');
-  saveCoverButton.classList.toggle('hidden');
+  homeButton.classList.add('hidden');
+  form.classList.add('hidden');
+  homeView.classList.remove('hidden');
+  randCoverButton.classList.remove('hidden');
+  saveCoverButton.classList.remove('hidden');
+}
+
+function viewSavedCovers() {
+  homeView.classList.add('hidden');
+  randCoverButton.classList.add('hidden');
+  makeCoverButton.classList.remove('hidden');
+  homeButton.classList.remove('hidden');
+  form.classList.add('hidden');
 }
 
 // We've provided two functions to get you started
